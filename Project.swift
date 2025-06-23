f5b5b086319de9095dc84037f83e611487ac8ec7
바견규 <bagyeongyu@bagyeongyuui-MacBookPro.local>
2025-06-24 01:02:21 +0900

    초기 설정/CI,CD 설정

--- Project.swift
+++ Project.swift
@@ -1,18 +1,23 @@
 import ProjectDescription
 
-let settings = Settings.settings(
-  base: [
-    "CODE_SIGN_STYLE": "Manual",
-    "CODE_SIGN_IDENTITY": "Apple Distribution",
-    "PROVISIONING_PROFILE_SPECIFIER": "match AppStore io.tuist.SOAPFT 1750692394",
-    "DEVELOPMENT_TEAM": "YOUR_TEAM_ID"
-  ]
-)
-
-
 let project = Project(
     name: "SOAPFT",
-    settings: settings,
+    settings: Settings.settings(
+        base: [
+            "DEVELOPMENT_TEAM": "YOUR_TEAM_ID",
+            "CODE_SIGN_STYLE": "Manual"
+        ],
+        configurations: [
+            .debug(name: "Debug", settings: [
+                "CODE_SIGN_IDENTITY": "Apple Development",
+                "PROVISIONING_PROFILE_SPECIFIER": "match Development io.tuist.SOAPFT"
+            ]),
+            .release(name: "Release", settings: [
+                "CODE_SIGN_IDENTITY": "Apple Distribution",
+                "PROVISIONING_PROFILE_SPECIFIER": "match AppStore io.tuist.SOAPFT 1750692394"
+            ])
+        ]
+    ),
     targets: [
         .target(
             name: "SOAPFT",
@@ -39,8 +44,7 @@ let project = Project(
             bundleId: "io.tuist.SOAPFTTests",
             infoPlist: .default,
             sources: ["SOAPFT/Tests/**"],
-            resources: [],
             dependencies: [.target(name: "SOAPFT")]
-        ),
+        )
     ]
 )
